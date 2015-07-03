ce35415e0ce01c3fcb4c27b6b418290ae390a278
ollitapa <olli.tapaninen@gmail.com>
2015-07-03 09:56:37 +0300

    More logging

--- Tests/meshLocalizationSpeedTest.py
+++ Tests/meshLocalizationSpeedTest.py
@@ -23,6 +23,18 @@ from mmpMeshSupport import convertPointDataToMesh
 from mupif import BBox, Field, FieldID, ValueType
 from datetime import datetime
 
+import logging
+logger = logging.getLogger('mmpraytracer')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# add formatter to ch
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
 if __name__ == '__main__':
 
     # Just test scripting here
