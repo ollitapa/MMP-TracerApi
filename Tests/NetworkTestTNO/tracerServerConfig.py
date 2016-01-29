572ee6fd564a966e9a48b7348bfcf159876b3a09
ollitapa <olli.tapaninen@gmail.com>
2016-01-29 13:11:15 +0200

    Server config for TNO test

---
+++ Tests/NetworkTestTNO/tracerServerConfig.py
@@ -0,0 +1,15 @@
+import os
+import Pyro4
+Pyro4.config.SERIALIZER = "pickle"
+Pyro4.config.PICKLE_PROTOCOL_VERSION = 2  # to work with python 2.x and 3.x
+Pyro4.config.SERIALIZERS_ACCEPTED = {'pickle'}
+
+
+server = '134.221.62.42'
+serverPort = 44387
+nshost = '134.221.62.42'  # NameServer - do not change
+nsport = 9092  # NameServer's port - do not change
+appName = "MMPRaytracer@mmpserver"
+hkey = ''  # Password for accessing nameServer and applications
+
+hostUserName = 'mmpwp2test'  # User name for ssh connection
